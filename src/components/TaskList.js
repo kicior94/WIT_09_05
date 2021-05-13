67fda4a7b88f773c694cedf6d5e61d87f3197470
kicior94 <tomekmarianski@gmail.com>
2021-05-13 18:00:33 +0200

    task list added

---
+++ src/components/TaskList.js
@@ -0,0 +1,37 @@
+const TaskList = () => {
+
+    const container = $(document.createElement('div'));
+    container.attr({
+        class: 'container'
+    });
+
+    const card = $(document.createElement('div'));
+    card.attr({
+        class: 'card'
+    });
+
+    const cardBody = $(document.createElement('div'));
+    cardBody.attr({
+        class: 'card-body'
+    });
+
+    const cardTitle = $(document.createElement('h5'));
+    cardTitle.attr({
+        class: 'card-title'
+    });
+    cardTitle.html('Task list');
+
+    const cardSubtitle = $(document.createElement('h6'));
+    cardSubtitle.attr({
+        class: 'card-subtitle mb-2 text-muted'
+    });
+    cardSubtitle.html('This is our task list');
+    
+    cardBody.append([cardTitle, cardSubtitle]);
+    card.append(cardBody);
+    container.append(card)
+
+    return container;
+}
+
+export default TaskList;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import Navigation from './components/Navigation.js';
 import TaskForm from './components/TaskForm.js';
+import TaskList from './components/TaskList.js';
 
 
 const root = $('#root');
 const navigation = Navigation('Navbar');
 const taskForm = TaskForm();
+const taskList = TaskList();
 root.append(navigation);
 root.append(taskForm);
+root.append(taskList);
